076c9eaf1802a5035b946dc5e5044a909c954245
Nitesh Patel <github@nitesh.name>
2015-04-19 17:53:43 +0100

    Bump version to 56

--- src/nitezh/ministock/Tools.java
+++ src/nitezh/ministock/Tools.java
@@ -44,7 +44,7 @@ import java.util.concurrent.Callable;
 
 public class Tools {
 
-    public static final String BUILD = "55";
+    public static final String BUILD = "56";
     private static final HashMap<String, String> mCurrencyCodeMap = new HashMap<String, String>();
     private static final HashMap<String, String> mCurrencyCharMap = new HashMap<String, String>();
 
