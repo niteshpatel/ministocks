937dacb017e951e01cabf6fc3a9990ceb553fca6
Nitesh Patel <github@nitesh.name>
2015-06-27 15:00:33 +0100

    Add missing version code update in VersionTools.java

--- src/nitezh/ministock/utils/VersionTools.java
+++ src/nitezh/ministock/utils/VersionTools.java
@@ -27,7 +27,7 @@ package nitezh.ministock.utils;
 
 public class VersionTools {
 
-    public static final String BUILD = "53";
+    public static final String BUILD = "57";
 
     private VersionTools() {
     }
