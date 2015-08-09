1d3e0f213b57707d19dccb694880ccf223de74d3
Peter Flanner <peter.flanner@gmail.com>
2015-08-08 21:07:10 -0400

    Update StockQuoteRepository to try to get the stock name from StockSuggestions if the name appears as "N/A"

--- src/nitezh/ministock/StockSuggestions.java
+++ src/nitezh/ministock/StockSuggestions.java
@@ -42,7 +42,7 @@ import nitezh.ministock.utils.StorageCache;
 import nitezh.ministock.utils.UrlDataTools;
 
 
-class StockSuggestions {
+public class StockSuggestions {
 
     private static final String BASE_URL = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?callback=YAHOO.Finance.SymbolSuggest.ssCallback&query=";
     private static final Pattern PATTERN_RESPONSE = Pattern.compile("YAHOO\\.Finance\\.SymbolSuggest\\.ssCallback\\((\\{.*?\\})\\)");
--- src/nitezh/ministock/domain/StockQuote.java
+++ src/nitezh/ministock/domain/StockQuote.java
@@ -148,4 +148,8 @@ public class StockQuote {
     public String getName() {
         return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
