01ed85b414024292865aec02d276b1d20e6eb68e
Nitesh Patel <github@nitesh.name>
2016-01-01 19:58:24 +0000

    Fix symbol suggestions (again) - need region and lang specifying

--- src/nitezh/ministock/StockSuggestions.java
+++ src/nitezh/ministock/StockSuggestions.java
@@ -40,7 +40,7 @@ import java.util.regex.Pattern;
 class StockSuggestions {
 
     private static final String BASE_URL =
-            "https://s.yimg.com/aq/autoc?callback=YAHOO.Finance.SymbolSuggest.ssCallback&region=&lang=&query=";
+            "https://s.yimg.com/aq/autoc?callback=YAHOO.Finance.SymbolSuggest.ssCallback&region=US&lang=en-US&query=";
     private static final Pattern PATTERN_RESPONSE =
             Pattern
                     .compile("YAHOO\\.Finance\\.SymbolSuggest\\.ssCallback\\((\\{.*?\\})\\)");
