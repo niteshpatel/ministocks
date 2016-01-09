388be8bdf8782174b655b399af14f0277d41f395
Nitesh Patel <github@nitesh.name>
2016-01-09 19:23:04 +0000

    Fix symbol suggestions again (need to specify region and language)

--- src/nitezh/ministock/StockSuggestions.java
+++ src/nitezh/ministock/StockSuggestions.java
@@ -44,7 +44,7 @@ import nitezh.ministock.utils.UrlDataTools;
 
 class StockSuggestions {
 
-    private static final String BASE_URL = "https://s.yimg.com/aq/autoc?callback=YAHOO.Finance.SymbolSuggest.ssCallback&region=&lang=&query=";
+    private static final String BASE_URL = "https://s.yimg.com/aq/autoc?callback=YAHOO.Finance.SymbolSuggest.ssCallback&region=US&lang=en-US&query=";
     private static final Pattern PATTERN_RESPONSE = Pattern.compile("YAHOO\\.Finance\\.SymbolSuggest\\.ssCallback\\((\\{.*?\\})\\)");
 
     public static List<Map<String, String>> getSuggestions(String query) {
