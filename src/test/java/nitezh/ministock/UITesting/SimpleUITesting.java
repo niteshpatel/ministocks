1707e9d65df391555a37290d32b339e5c508977f
GurkomalSRao <gsrao@hotmail.ca>
2018-03-14 18:55:51 -0400

    #20 Graphical View Test File
    Test file that validates if a properly contructed graphical view will be
    displayed. The URL will validate if proper data is being trasmitted.
    Validated using response codes (200 for correct 400 for incorrect)
    
    Created Sample file for testing UI. Actual implementation will be added
    afterwards

---
+++ src/test/java/nitezh/ministock/UITesting/SimpleUITesting.java
@@ -0,0 +1,23 @@
+package nitezh.ministock.UITesting;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by GRao on 3/14/2018.
+ */
+
+public class SimpleUITesting {
+
+    @Before
+    public void setup(){
+
+    }
+
+    @Test
+    public void UserInterfaceTesting(){
+        /*TODO
+        ADD TESTING FOR USER INTERFACE
+         */
+    }
+}
