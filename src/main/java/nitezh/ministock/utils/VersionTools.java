7f03691d1240ac46844a2dc2dea528c16706446d
Nitesh Patel <nitesh.patel@hotmail.com>
2019-05-11 14:23:52 +0100

    Bump build version

--- src/main/java/nitezh/ministock/utils/VersionTools.java
+++ src/main/java/nitezh/ministock/utils/VersionTools.java
@@ -27,7 +27,7 @@ package nitezh.ministock.utils;
 
 public class VersionTools {
 
-    public static final String BUILD = "76";
+    public static final String BUILD = "77";
 
     private VersionTools() {
     }
